6c5a48158b95b6164b25b83ea9fc6da4fa612996
OnePoint-bijay <OnePoint-bijay@DESKTOP-KH5S7CP>
2025-01-07 13:12:32 +0545

    Basic Refactor

--- src/TaskManagementApplicationTests/obj/Debug/net7.0/Testing.AssemblyInfo.cs
+++ src/TaskManagementApplicationTests/obj/Debug/net7.0/Testing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73ab31c883c384959034f63119904ca13762ec30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2eecd34154ac5eafe94de0993f04b9d08569c02")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
